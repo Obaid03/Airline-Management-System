5f72c1ab48eae53a3755993660cbc81c9cfd7d84
maybethemuhammadibrahim <mibrahimfiftysix@gmail.com>
2025-03-23 00:59:28 +0500

    create classes

--- User.h
+++ User.h
@@ -4,6 +4,10 @@
 
 #ifndef USER_H
 #define USER_H
+#include "Flight.h"
+#include <vector>
+using namespace std;
+
 
 
 class User {
@@ -18,5 +22,79 @@ public:
     
 
 };
+
+
+
+class Admin : public User {
+private:
+
+public:
+Admin(string userID, string userName, string userEmail, string userPassword) : User(userID,userName,userEmail,userPassword) {}
+   
+
+    void addNewFlight() {
+
+    }
+
+    void removeFlight() {
+
+    }
+
+    void listAllFlights() {
+
+    }
+
+    void listAllCustomers() {
+
+    }
+};
+
+
+class Customer : public User {
+public:
+    static int noOfCustomers;
+    vector<Flight> flightsRegisteredByUser;
+    int noOfFlights = flightsRegisteredByUser.size();
+
+    vector<Flight> flightsBookedByUser;
+    int noOfBookedFlights = flightsBookedByUser.size();
+
+
+private:
+    Customer(string userID, string userName, string userEmail, string userPassword) : User(userID,userName,userEmail,userPassword) , flightsRegisteredByUser(NULL) {
+        noOfCustomers++;
+    }
+
+    void editInformation() {
+
+    }
+
+
+    void displayData() {
+
+    }
+
+    void bookNewFlight() {
+
+    }
+
+    void removeFlightFromBooking() {
+
+    }
+
+    //when booking is confirmed
+    void addNewFlight() {
+
+    }
+
+    void removeFlight() {
+
+    }
+
+
+
+};
+
+int Customer::noOfCustomers = 0;
 
 #endif //USER_H
