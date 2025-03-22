c137019a26483eed1873d5f22941c24e9c3895d5
maybethemuhammadibrahim <mibrahimfiftysix@gmail.com>
2025-03-23 00:41:36 +0500

    code

--- User.h
+++ User.h
@@ -8,8 +8,15 @@
 
 class User {
 private:
+    string userID;
+    string userName;
+    string userEmail;
+    string userPassword;
 
 public:
+    User(string userID, string userName, string userEmail, string userPassword) : userID(userID), userName(userName), userEmail(userEmail), userPassword(userPassword) {};
+    
+
 };
 
 #endif //USER_H
