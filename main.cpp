c7c0dc255c8787f6af0db8f06e30cf78f79e1e6a
maybethemuhammadibrahim <mibrahimfiftysix@gmail.com>
2025-03-23 01:08:42 +0500

    test menu

--- main.cpp
+++ main.cpp
@@ -16,6 +16,43 @@ git push origin main
 #include "User.h"
 using namespace std;
 
+
+void addUser() {
+    
+}
+
+void menu() {
+    cout << "Testing" << endl;
+    int temp;
+    cout << "1.Admin, 2.User";
+    cin >> temp;
+    switch (temp)
+    {
+    case 1:
+        /* code */
+        break;
+    
+    case 2:
+        cout << "1.Register new User\n2.Login User" ;
+        cin >> temp;
+        switch (temp)
+        {
+        case 1:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }    
+    
+    default:
+        break;
+    }
+    
+    
+}
+
+
 int main() {
     cout << "Hello World" << endl;
 }
